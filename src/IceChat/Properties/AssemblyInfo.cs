90d0c4f308e7ef328099402e3894bc4f5025704f
icechat <snerf@icechat.net>
2017-08-30 19:43:12 -0700

    Update Build Number

--- src/IceChat/Properties/AssemblyInfo.cs
+++ src/IceChat/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("9.16.0.0")]
-[assembly: AssemblyFileVersion("9.16.2017.0815")]
+[assembly: AssemblyVersion("9.17.0.0")]
+[assembly: AssemblyFileVersion("9.17.2017.0830")]
